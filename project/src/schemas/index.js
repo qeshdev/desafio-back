85db5058905a919ef70d7b5e4157e2eee23f014e
Camila Dornas <camiladornas@gmail.com>
2022-05-29 15:14:39 -0300

    feat: estruturação básica da pasta SRC para inicialização do código

---
+++ project/src/schemas/index.js
@@ -0,0 +1,1 @@
+// const Joi = require('@hapi/joi');
---
+++ project/src/utils/statusCode.js
@@ -0,0 +1,12 @@
+module.exports = {
+  success: 200,
+  created: 201,
+  noContent: 204,
+  badRequest: 400,
+  unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
+  conflict: 409,
+  unprocessableEntity: 422,
+  internalError: 500,
+};
