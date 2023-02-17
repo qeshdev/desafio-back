2535849be5bfea0d633ad2004f85b520dc93d4e3
norberto-nerit <norberto@nerit.com.br>
2023-02-17 09:21:43 -0300

    fix : ajustando menu

--- project/veiculos-ui/pages/common/components/Header.tsx
+++ project/veiculos-ui/pages/common/components/Header.tsx
@@ -1,7 +1,8 @@
-import { Component, ReactNode } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import styles from '../../../styles/main.module.css'
 import Script from 'next/script'
+import { Component, ReactNode } from 'react'
+import styles from '../../../styles/main.module.css'
+import { LayoutEnum } from './enum/LayoutEnum'
 
 export default class Header extends Component {
 
@@ -14,29 +15,41 @@ export default class Header extends Component {
         }
     }
 
+    getLayout(layout: LayoutEnum) {
+
+        switch (layout) {
+
+            case LayoutEnum.AUTOCAR:
+                return window.location.assign(LayoutEnum.AUTOCAR)
+
+            case LayoutEnum.AUTH_LOGIN:
+                return window.location.assign(LayoutEnum.AUTH_LOGIN)
+        }
+
+    }
+
     render(): ReactNode {
         return (
             <>
                 <header className={`container-fluid fixed-top ${styles['l-box-header']}`}>
                     <nav className={`navbar navbar-expand-lg navbar-light bg-light`}>
                         <div className={`container-fluid ${styles['l-box-nav']}`}>
-                            <a className={`navbar-brand ${styles['c-text-menu']}`} href="autocar">@AutoCar</a>
+                            <a className={`navbar-brand ${styles['c-text-menu']}`} onClick={()=>this.getLayout(LayoutEnum.AUTOCAR)}>@AutoCar</a>
                             <button className={`${styles['nav-btn']}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNav">
                                 <ul className="navbar-nav">
                                     <li className="nav-item">
-                                        <a className={`nav-link active ${styles['c-text-menu']}`} aria-current="page" href="autocar">Home</a>
+                                        <a className={`nav-link active ${styles['c-text-menu']}`} aria-current="page" onClick={()=>this.getLayout(LayoutEnum.AUTOCAR)}>Home</a>
                                     </li>
                                     <li className="nav-item">
-                                        <a className={`nav-link ${styles['c-text-menu']}`} href="auth/login">Cadastre seu imovel</a>
+                                        <a className={`nav-link ${styles['c-text-menu']}`} onClick={()=>this.getLayout(LayoutEnum.AUTH_LOGIN)}>Cadastre seu imovel</a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </nav>
-                    <img src="imgs/logo.png" className="logo" />
                 </header>
 
                 <Script
